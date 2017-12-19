eca8191ed27b3a8e5039d9f978db7f288a8ef87b
Vishal Rangineni <vr345@afsconnect1.njit.edu>
2017-12-18 20:04:13 -0500

    modified edit_accounts

--- pages/edit_account.php
+++ pages/edit_account.php
@@ -22,7 +22,7 @@
     First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
     Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
     Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
-    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
+    Phone number: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
     Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
     Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br><br>
     <button type="submit" form="form1" value="Submit">Save</button>
